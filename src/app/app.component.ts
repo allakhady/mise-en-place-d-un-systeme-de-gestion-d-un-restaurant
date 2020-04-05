62d0de33d6975e8b16832fefa8cbeeed41954c1b
allakhady <allafaye717@gmail.com>
2020-04-05 02:06:32 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent implements OnInit {
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Accueil',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'Nos plats',
+      url: '/plats',
+      icon: 'restaurant'
+    },
+    {
+      title: 'Menu du jour',
+      url: '/menu',
+      icon: 'pizza'
+    },
+    {
+      title: 'Mes commandes',
+      url: '/plats/commande',
+      icon: 'cart'
+    },
+    {
+      title: 'Se connecter',
+      url: '/folder/Archived',
+      icon: 'log-in'
+    }
+  ];
+  public labels = ['Notes', 'Reminders'];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  ngOnInit() {
+    const path = window.location.pathname.split('folder/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+  }
+}
